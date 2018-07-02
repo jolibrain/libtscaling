2d675529858be06b4f9d7fb1a845aa5ba1b0f13d
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2018-07-02 12:47:12 +0200

    add utility to split lmdb (in orderto split test lmdb into test.tmbd and val.lmdb

---
+++ utils/split_lmdb.py
@@ -0,0 +1,39 @@
+import argparse
+import lmdb
+import random
+import sys
+
+parser = argparse.ArgumentParser(description='LMDB splitter')
+parser.add_argument('--input',help='input lmdb')
+parser.add_argument('--test-output',help='output lmdb (for test)')
+parser.add_argument('--val-output',help='output lmdb (for validation)')
+parser.add_argument('--test-prop', help='test proportion (val proportion is 1-test_prop)',type=float)
+args = parser.parse_args()
+
+
+
+db = lmdb.open(args.input,readonly=True)
+txn = db.begin()
+cursor = txn.cursor()
+data = []
+for key, value in cursor:
+    data.append([key, value])
+
+all_indexes = set(xrange(len(data)))
+test_indexes = random.sample(all_indexes,int(float(len(data))*args.test_prop))
+val_indexes = list(all_indexes - set(test_indexes))
+
+
+map_size = (sys.getsizeof(data[0][0]) + sys.getsizeof(data[0][1])) * len(data)
+
+test_db = lmdb.open(args.test_output, map_size = map_size)
+with test_db.begin(write=True) as txn:
+    for i in test_indexes:
+        txn.put(data[i][0], data[i][1])
+
+val_db = lmdb.open(args.val_output, map_size=map_size)
+with val_db.begin(write=True) as txn:
+    for i in val_indexes:
+        txn.put(data[i][0], data[i][1])
+
+
